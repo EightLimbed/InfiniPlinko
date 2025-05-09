91ff391a1b2c76d9206411c63f38b9d51488c5aa
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2025-05-08 20:05:17 -0400

    Added Pegs

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 
 var random = RandomNumberGenerator.new()
+var speed : int = 0
 
 func _ready() -> void:
 	left()
@@ -12,6 +13,10 @@ func right():
 	play("Right")
 
 func _on_animation_finished() -> void:
+	if random.randi_range(0,10) != 0:
+		speed_scale += 0.1
+	else:
+		speed_scale /= 2.0
 	if get_animation() == "Right":
 		position.x += 64
 	if get_animation() == "Left":
