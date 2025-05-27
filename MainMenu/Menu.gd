39345ab1de7042022cb85de5017b14995de9da62
DnumdeYevoc <163893629+DnumdeYevoc@users.noreply.github.com>
2025-05-26 20:19:51 -0700

    Finished menu

--- MainMenu/Menu.gd
+++ MainMenu/Menu.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
-
+@onready var Graph = $"Info/Probability Distrubution"
+@onready var info = $Info
 func _on_button_pressed() -> void:
 	if str($VBoxContainer/Label3/TextEdit.text).is_valid_int():
 		var instance = load("res://Main/Game.tscn").instantiate()
@@ -7,3 +8,21 @@ func _on_button_pressed() -> void:
 		instance.risk = $VBoxContainer/Label2/HSlider.value
 		get_tree().root.add_child(instance)
 		queue_free()
+
+
+func _on_info_button_pressed() -> void:
+	info.visible = not info.visible
+	if info.visible == true:
+		$InfoButton.text = "Home"
+	else:
+		$InfoButton.text = "Info"
+
+
+func _on_graph_button_pressed() -> void:
+	Graph.visible = not Graph.visible
+	if Graph.visible == true:
+		$"Info/Calc/Graph Button".text = "Back"
+		$InfoButton.visible = false
+	else:
+		$"Info/Calc/Graph Button".text = "Probability Distrubution Example"
+		$InfoButton.visible = true
