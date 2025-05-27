1f7f426d92c3aad4836dcaeb89ea022aca4d0a32
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2025-05-27 19:31:09 -0400

    Presets

--- MainMenu/WinScreen.gd
+++ MainMenu/WinScreen.gd
@@ -8,7 +8,7 @@ func win(value):
 	if ResourceLoader.exists("user://SavedGames.tres"):
 		save_file = ResourceLoader.load("user://SavedGames.tres")
 	save_file.saved_bets.append(game.bet)
-	save_file.saved_profits.append(round(value*100.0)/100.0-game.bet)
+	save_file.saved_profits.append(abs(round(value*100.0)/100.0)-game.bet)
 	ResourceSaver.save(save_file,"user://SavedGames.tres")
 	if value < 0:
 		$VBoxContainer/Label3.text = "Shucks! You Lost $"+str(abs(round(value*100.0)/100.0)-game.bet)
